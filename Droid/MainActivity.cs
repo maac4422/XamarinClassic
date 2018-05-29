466cd5db0e1d5589a05ffdd129f8f3affb123dc5
miguel.arenas <miguel.arenas@MIGUEL-ARENAS>
2018-05-29 09:51:43 -0500

    Android:Create tab page and navigation between tabs

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,14 +1,17 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Support.Design.Widget;
+using System;
+using Android.Support.V4.App;
+using UFCApp.Droid.Fragments;
 
 namespace UFCApp.Droid
 {
     [Activity(Label = "UFCApp", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : Activity
+    public class MainActivity : FragmentActivity
     {
-        int count = 1;
-
+        TabLayout tabLayout;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -16,11 +19,30 @@ namespace UFCApp.Droid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            tabLayout = FindViewById<TabLayout>(Resource.Id.mainTabLayout);
+            tabLayout.TabSelected += TabLayoutTabSelected;
 
-            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+            FragmentNavigate(new NewsFragment());
+        }
+
+        private void TabLayoutTabSelected(object sender, TabLayout.TabSelectedEventArgs e)
+        {
+            switch (e.Tab.Position)
+            {
+                case 0:
+                    FragmentNavigate(new NewsFragment());
+                    break;
+                case 1:
+                    FragmentNavigate(new EventsFragment());
+                    break;
+            }
+        }
+
+        private void FragmentNavigate(Android.Support.V4.App.Fragment fragment)
+        {
+            var transaction = SupportFragmentManager.BeginTransaction();
+            transaction.Replace(Resource.Id.contentFrame, fragment);
+            transaction.Commit();
         }
     }
 }
