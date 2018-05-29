360b67eb9cad66ffd6e947f4c143c4ca0c31fb7a
miguel.arenas <miguel.arenas@MIGUEL-ARENAS>
2018-05-29 10:55:24 -0500

    Start create project architecture and some class for recycler views

---
+++ ServiceAccessLayer/Services/ApiServices.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiceAccessLayer.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Models.Models;
+    using Newtonsoft.Json;
+    using Plugin.Connectivity;
+
+    public class ApiServices
+    {
+    }
+}
---
+++ ServiceAccessLayer/Services/EventsServices.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceAccessLayer.Services
+{
+    public class EventsServices
+    {
+    }
+}
