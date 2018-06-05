1c4c3e874a3a4985876eb89357aeb1a61739aaeb
miguel.arenas <miguel.arenas@MIGUEL-ARENAS>
2018-06-05 07:24:26 -0500

    Start create drawable menu

--- Droid/Activities/MainActivity.cs
+++ Droid/Activities/MainActivity.cs
@@ -1,16 +1,20 @@
 ﻿namespace UFCApp.Droid
 {
+    using System;
     using Android.App;
     using Android.OS;
     using Android.Support.Design.Widget;
-    using Android.Support.V4.App;
+    using Android.Support.V7.App;
+    using Android.Support.V7.Widget;
     using UFCApp.Droid.Fragments;
+    using SupportToolbar = Android.Support.V7.Widget.Toolbar;
 
     [Activity(Label = "UFCApp", MainLauncher = true, Icon = "@mipmap/icon")]
-    public class MainActivity : FragmentActivity
+    public class MainActivity : AppCompatActivity
     {
         #region Attributes
         private TabLayout tabLayout;
+        private SupportToolbar toolbar;
         #endregion
 
         #region LifeCycle
@@ -20,35 +24,20 @@
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
-
-            tabLayout = FindViewById<TabLayout>(Resource.Id.mainTabLayout);
-            tabLayout.TabSelected += TabLayoutTabSelected;
-
-            FragmentNavigate(new NewsFragment());
+            //toolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            //SetSupportActionBar(toolbar);
+           
         }
+
+
         #endregion
 
         #region Methods
-        private void TabLayoutTabSelected(object sender, TabLayout.TabSelectedEventArgs e)
-        {
-            switch (e.Tab.Position)
-            {
-                case 0:
-                    FragmentNavigate(new NewsFragment());
-                    break;
-                case 1:
-                    FragmentNavigate(new EventsFragment());
-                    break;
-            }
-        }
+        
 
-        private void FragmentNavigate(Android.Support.V4.App.Fragment fragment)
-        {
-            var transaction = SupportFragmentManager.BeginTransaction();
-            transaction.Replace(Resource.Id.contentFrame, fragment);
-            transaction.Commit();
-        }
+        
         #endregion
+    
     }
 }
 
---
+++ Droid/Activities/NavigationFragmentActivity.cs
@@ -0,0 +1,53 @@
+﻿namespace UFCApp.Droid.Activities
+{
+    using Android.App;
+    using Android.OS;
+    using Android.Support.Design.Widget;
+    using Android.Support.V4.App;
+    using UFCApp.Droid.Fragments;
+
+    [Activity(Label = "NavigationFragmentActivity")]
+    public class NavigationFragmentActivity : FragmentActivity
+    {
+        #region Attributes
+        private TabLayout tabLayout;
+        #endregion
+
+        #region LifeCycle
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.NavigationTab);
+
+            tabLayout = FindViewById<TabLayout>(Resource.Id.mainTabLayout);
+            tabLayout.TabSelected += TabLayoutTabSelected;
+
+            FragmentNavigate(new NewsFragment());
+        }
+        #endregion
+
+        #region Methods
+        private void TabLayoutTabSelected(object sender, TabLayout.TabSelectedEventArgs e)
+        {
+            switch (e.Tab.Position)
+            {
+                case 0:
+                    FragmentNavigate(new NewsFragment());
+                    break;
+                case 1:
+                    FragmentNavigate(new EventsFragment());
+                    break;
+            }
+        }
+
+        private void FragmentNavigate(Android.Support.V4.App.Fragment fragment)
+        {
+            var transaction = SupportFragmentManager.BeginTransaction();
+            transaction.Replace(Resource.Id.contentFrame, fragment);
+            transaction.Commit();
+        }
+        #endregion
+    }
+}
